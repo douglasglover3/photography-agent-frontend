9022d77e496ac7c60df20669e245f3fb5a20610e
douglasglover3 <douglasglover3@email.com>
2025-11-15 20:15:17 -0500

    github pages changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		})
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/photography-agent-frontend' : ''
+    }
+  }
 };
-
-export default config;
